d58b9e01f0c704dbf58a62faa2a40ec03477033a
Xyrusmarr <127576138+Xyrusmarr@users.noreply.github.com>
2025-10-05 08:38:39 -0400

    Create releasetools.py

---
+++ releasetools.py
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: The Android Open Source Project
+# SPDX-FileCopyrightText: The MoKee Open Source Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import common
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def AddImage(info, basename, dest):
+  name = basename
+  data = info.input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def OTA_InstallEnd(info):
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+  return
