04864c5c0df73eba56bbab19a5f631ba81f1dc56
Xyrusmarr <127576138+Xyrusmarr@users.noreply.github.com>
2025-11-16 16:40:52 -0500

    Fix device file paths and update product name

--- lineage_T801S.mk
+++ lineage_T801S.mk
@@ -9,13 +9,13 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from t801s device
-$(call inherit-product, device/tcl/t801s/device.mk)
+$(call inherit-product, device/tcl/T801s/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_T81S
+PRODUCT_NAME := lineage_T801S
 PRODUCT_DEVICE := T801S
 PRODUCT_BRAND := TCL
 PRODUCT_MANUFACTURER := TCL
@@ -23,5 +23,5 @@ PRODUCT_MANUFACTURER := TCL
 PRODUCT_GMS_CLIENTID_BASE := android-T801S
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="T801S-user Ottawa11 SKQ1.211006.001 5EGM release-keys" \
-    BuildFingerprint=TCL/T801S/Ottawa11/SKQ1.211001.001/5EGM:user/release-keys
+    BuildDesc="T801S-user 12 SKQ1.211006.001 5EGM release-keys" \
+    BuildFingerprint=TCL/T801S/Ottawa11:12/SKQ1.211001.001/5EGM:user/release-keys
